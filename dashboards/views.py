6679362baef61794549ae6544a825cedfc7c4db8
GabrielTI2001 <damatagabriel1011@gmail.com>
2024-02-19 09:25:38 -0300

    Update 19.02.2024

--- dashboards/views.py
+++ dashboards/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from sales.models import Venda
 from django.db.models import Sum
+import requests
 
 def dashboard_vendas(request):
     ranking = Venda.objects.values('cliente', 'cliente__razao_social').annotate(
