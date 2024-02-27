bcc5197f6edfec6f5978a7f596205b015fde7ffc
GabrielTI2001 <damatagabriel1011@gmail.com>
2024-02-27 17:55:19 -0300

    Update

--- users/signals.py
+++ users/signals.py
@@ -3,11 +3,11 @@ from django.contrib.auth.models import User, Group # Import the built-in User mo
 from django.dispatch import receiver # Import the receiver
 from .models import Profile
 
-# @receiver(post_save, sender=User)
-# def add_to_default_group(sender, instance, created, **kwargs):
-#     if created:
-#         default_group = Group.objects.get(name='Colaborador') 
-#         instance.groups.add(default_group)
+@receiver(post_save, sender=User)
+def add_to_default_group(sender, instance, created, **kwargs):
+    if created:
+        default_group = Group.objects.get(name='Colaborador') 
+        instance.groups.add(default_group)
 
 @receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
